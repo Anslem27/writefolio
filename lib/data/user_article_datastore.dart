b6c83b4bc5b6f0dd8c56272e7ef9e63c532a9b24
Anslem27 <anslembarn@gmail.com>
2023-03-12 13:52:23 +0300

    added article user datastor functions

--- lib/data/user_article_datastore.dart
+++ lib/data/user_article_datastore.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:writefolio/models/articles/article.dart';
 
@@ -7,5 +8,21 @@ class UserArticleDataStore {
 
   Future<void> saveArticle({required UserArticle userArticle}) async {
     await box.put(userArticle.id, userArticle);
+  }
+
+  Future<void> updateArticle({required UserArticle savedArticle}) async {
+    await savedArticle.save();
+  }
+
+  ValueListenable<Box<UserArticle>> listenToSavedArticles() {
+    return Hive.box<UserArticle>(boxName).listenable(); //box.listenable();
+  }
+
+  Future<void> deleteSavedArticle({required UserArticle savedArticle}) async {
+    await savedArticle.delete();
+  }
+
+  Future<UserArticle?> getPoem({required String savedArticleId}) async {
+    return box.get(savedArticleId);
   }
 }
