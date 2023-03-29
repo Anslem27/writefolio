adaaa19bbe05ece2c4cf0c6b72d7910159a05bd1
Anslem27 <anslembarn@gmail.com>
2023-03-29 20:10:19 +0300

    improved poem's service

--- lib/services/poem_service.dart
+++ lib/services/poem_service.dart
@@ -4,8 +4,10 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class PoemService {
+  static int poemCount = 20;
+  //random
   static Future<HomePoemList> fetchHome() async {
-    var url = Uri.parse('https://poetrydb.org/random/20');
+    var url = Uri.parse('https://poetrydb.org/random/$poemCount');
     final response = await http.get(url);
 
     if (response.statusCode == 200) {
@@ -18,13 +20,53 @@ class PoemService {
 
   //search by author
   static Future<HomePoemList> fetchSearch(String query) async {
-    var url = Uri.parse("https://poetrydb.org/author,poemcount/$query;20");
+    var url =
+        Uri.parse("https://poetrydb.org/author,poemcount/$query;$poemCount");
 
     final response = await http.get(url);
     if (response.statusCode == 200) {
       return HomePoemList.fromJson(json.decode(response.body));
     } else {
       logger.w("Failed to fetch poems");
+      throw Exception('Failed to fetch poems');
+    }
+  }
+
+  //search by title {or theme}
+  static Future<HomePoemList> searchByTitle(String query) async {
+    var url = Uri.parse("https://poetrydb.org/title/$query");
+
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      return HomePoemList.fromJson(json.decode(response.body));
+    } else {
+      logger.w("Failed to fetch poems");
+      throw Exception('Failed to fetch poems');
+    }
+  }
+
+  //search by line from poem
+  static Future<HomePoemList> searchByPoemLine(String query) async {
+    var url = Uri.parse("https://poetrydb.org/lines/$query");
+
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      return HomePoemList.fromJson(json.decode(response.body));
+    } else {
+      logger.w("Failed to fetch poems");
+      throw Exception('Failed to fetch poems by line');
+    }
+  }
+
+  //query sonnets
+  static Future<HomePoemList> fetchSonnets() async {
+    var url = Uri.parse("https://poetrydb.org//title,random/Sonnet;$poemCount");
+
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      return HomePoemList.fromJson(json.decode(response.body));
+    } else {
+      logger.w("Failed to fetch Sonnets");
       throw Exception('Failed to fetch poems');
     }
   }
