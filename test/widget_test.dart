6721a93ad05f07312c13bfdf0e5a607bbb2a7bc2
Anslem27 <anslembarn@gmail.com>
2023-05-05 10:44:03 +0300

    chore(refactored tests)

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:writefolio/main.dart';
+import 'package:writefolio/writefolio_app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
